4bc2f1636b984ef3f415675b154509d4ee595e51
G-mirna <rivasmirna1997m@gmail.com>
2024-01-21 11:30:24 -0600

    Inicio de clase UNIVO

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MirnaR</title>
+</head>
+<body>
+    
+    <form action="" method="POST" role="form">
+        <legend>Informacion Personal</legend>
+        <br>
+        <br>
+    
+        <div class="form-group">
+            <label for="">Nombre:</label>
+            <input type="text" class="form-control" id="" placeholder="Ingrese su nombre">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="">Apellidos:</label>
+            <input type="text" class="form-control" id="" placeholder="Ingrese su apellidos">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="">Edad:</label>
+            <input type="text" class="form-control" id="" placeholder="Ingrese su edad">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="">Direccion:</label>
+            <input type="text" class="form-control" id="" placeholder="Ingrese su dirección">
+        </div>
+    
+        <br>
+    
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    
+</body>
+</html>
